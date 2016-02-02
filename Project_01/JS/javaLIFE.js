b85d0b8560321f1708ae6e6c10955d9d309cfa99
Jennifer W <w.jennifer123@gmail.com>
2016-02-02 12:13:58 -0800

    spin click works

---
+++ Project_01/JS/javaLIFE.js
@@ -0,0 +1,34 @@
+/*Javascript Functions:
+	- Interactive players (x2)
+	- Randomizer for spin dial
+	- Money score/counter
+	- Be able to move players to number of spaces according to spin dial
+	- Event trigger for boxes
+	- Event needs to be able to deplete/add specific player's money
+	- Pop-up for  beginning game, ending game. (ie: 'PLAYER 2' WINS)
+*/
+
+var playerOne = '<img src="CSS/IMG/Hipster Girl.png" alt="Girl">'
+var playerTwo = '<img src="CSS/IMG/Cooldude.png" alt="Boy">'
+var moneyScore
+var spaces = document.querySelectorAll('.spaces')
+var spinDial = document.querySelector('.spin')
+var display = document.querySelector('#display')
+
+// function start() {
+document.querySelector('.spin').addEventListener("click", function(getRandom) {
+    alert("Let's begin!");
+    console.log("workkk");
+  })
+  function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+// }
+
+
+
+// for (var i = 0; i < spinDial.length; i++) {
+//   spinDial[i].addEventListener('click', start)
+//   console.log("forloop spin")
+//   }
