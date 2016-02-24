afc4f087a74ce018f9098c98af8d8e60cfd114f9
Jennifer W <w.jennifer123@gmail.com>
2016-02-23 18:39:39 -0800

    events edit and show are working

--- 02_Project/moody_events/app/controllers/events_controller.rb
+++ 02_Project/moody_events/app/controllers/events_controller.rb
@@ -1,14 +1,18 @@
 class EventsController < ApplicationController
+
   def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
   end
 
   def create
-  end
-
-  def show
   end
 
   def edit
